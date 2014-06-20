821722e8c3d64df8ddc0863c4cc492db4a4a1bee
Julio César Guzmán <julio.guzman@me.com>
2014-06-19 19:16:35 -0500

    no sé como hacer herencia aquí...

--- Meme Wars/GameScene.swift
+++ Meme Wars/GameScene.swift
@@ -8,25 +8,46 @@
 
 import SpriteKit
 
+class Spaceship : SKSpriteNode
+{
+    var lifeText: String!
+    var life: Int
+    {
+        get{ return self.life }
+        set{ lifeText = "❤️\(newValue)%" }
+    }
+    
+    
+    init(imageNamed name: String!)
+    {
+        super.init(imageNamed: name )
+    }
+}
+
+
 class GameScene: SKScene
 {
     var player: SKSpriteNode!
     
     override func didMoveToView(view: SKView)
     {
+        
+        self.backgroundColor = UIColor.whiteColor()
+        
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         let midscreen = CGPoint(x:200.0, y:100.0);
         myLabel.text = "100%";
         myLabel.fontSize = 65;
         myLabel.position = midscreen
-        
+        myLabel.fontColor = UIColor.blackColor()
+        //player = Spaceship()
         player = SKSpriteNode(imageNamed:"Spaceship")
-            
-        player.xScale = 0.25
-        player.yScale = 0.25
+        player.xScale = 0.15
+        player.yScale = 0.15
         player.position = midscreen
-
+        Spaceship(imageNamed:"Spaceship")
+       
         player.addChild(myLabel)
         self.addChild(player)
     }
@@ -48,12 +69,10 @@ class GameScene: SKScene
     
     override func touchesMoved(touches: NSSet, withEvent event: UIEvent)
     {
-        for touch: AnyObject in touches {
+        for touch: AnyObject in touches
+        {
             let location = touch.locationInNode(self)
-            
             player.runAction(SKAction.moveTo(location,duration: 0.1))
-            //SKAction.followPath(path: CGPath!, speed: 1.0)
-            //sprite.runAction(SKAction.repeatActionForever(action))
         }
     }
    
